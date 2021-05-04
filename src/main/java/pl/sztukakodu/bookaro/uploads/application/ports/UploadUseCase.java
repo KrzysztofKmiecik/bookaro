2339618c40a95f38c28a59da8f80b789937de1c0
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-05-04 18:04:21 +0200

    M4L13-Serwowanie Plikow

--- src/main/java/pl/sztukakodu/bookaro/uploads/application/ports/UploadUseCase.java
+++ src/main/java/pl/sztukakodu/bookaro/uploads/application/ports/UploadUseCase.java
@@ -3,9 +3,12 @@ package pl.sztukakodu.bookaro.uploads.application.ports;
 import lombok.Value;
 import pl.sztukakodu.bookaro.uploads.domain.Upload;
 
+import java.util.Optional;
+
 public interface UploadUseCase {
 
     Upload save(SaveUploadCommand command);
+    Optional<Upload> getById(String id);
 
     @Value
     class SaveUploadCommand {
