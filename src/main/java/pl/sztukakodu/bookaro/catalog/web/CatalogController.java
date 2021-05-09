b33ddc115122c8a6f58f114dcc580817d5342441
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-05-09 20:46:17 +0200

    M4L15-Swagger

--- src/main/java/pl/sztukakodu/bookaro/catalog/web/CatalogController.java
+++ src/main/java/pl/sztukakodu/bookaro/catalog/web/CatalogController.java
@@ -4,6 +4,7 @@ package pl.sztukakodu.bookaro.catalog.web;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -72,7 +73,7 @@ public class CatalogController {
 
     }
 
-    @PutMapping("/{id}/cover")
+    @PutMapping(value = "/{id}/cover",consumes ={MediaType.MULTIPART_FORM_DATA_VALUE} )
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void addBookCover(@PathVariable Long id, @RequestParam("file") MultipartFile file) throws IOException {
         System.out.println("Got file: " + file.getOriginalFilename());
